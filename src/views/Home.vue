c06e1bbcde64c8792222f73d3a66729ac51bebd0
jovandosen <jovandosen1994@gmail.com>
2020-06-08 19:16:30 +0200

    Vue travel app updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,4 +43,11 @@ img {
     display: flex;
     justify-content: space-between;
 }
+a {
+    color: lightseagreen;
+    text-decoration: none;
+}
+a:hover, a:visited {
+    text-decoration: underline;
+}
 </style>
