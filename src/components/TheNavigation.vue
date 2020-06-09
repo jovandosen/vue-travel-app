f2104e9dc9072821f70b382a10aab077ebdcf6a4
jovandosen <jovandosen1994@gmail.com>
2020-06-09 13:44:19 +0200

    Nested routes added

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -6,7 +6,7 @@
                 <router-link to="/">Home</router-link>
             </li>
             <li v-for="destination in destinations" :key="destination.name" class="links">
-                <router-link :to="{ name: 'DestinationDetails', params: { id: destination.id } }">
+                <router-link :to="{ name: 'DestinationDetails', params: { slug: destination.slug } }">
                     {{ destination.name }}
                 </router-link>
             </li>
@@ -19,7 +19,6 @@ import store from "@/store.js"
 export default {
    data() {
         return {
-            destinationId: this.$route.params.id,
             destinations: store.destinations
         }
    } 
--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,27 @@ const routes = [
     {
         path: "/",
         name: "Home",
-        component: Home
+        component: Home,
+        props: true
     },
     {
-        path: "/details/:id",
+        path: "/destination/:slug",
         name: "DestinationDetails",
-        component: () => import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails")
+        props: true,
+        component: () => import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails"),
+        children: [
+            {
+                path: ":experienceSlug",
+                name: "experienceDetails",
+                props: true,
+                component: () => import(/* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails")
+            }
+        ]
     }
 ];
 
 const router = new VueRouter({
+    mode: "history",
     linkExactActiveClass: "vue-school-active-class",
     routes
 });
