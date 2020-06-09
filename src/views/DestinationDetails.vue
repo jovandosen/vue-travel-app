0387eaf7802d67dc7e083992f7cf0dced83cc5f9
jovandosen <jovandosen1994@gmail.com>
2020-06-09 14:17:55 +0200

    Go Back button added and Transitions added

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <GoBack />
         <section class="destination">
             <h1>{{ destination.name }}</h1>
             <div class="destination-details">
@@ -25,8 +26,12 @@
 </template>
 
 <script>
-import store from "@/store.js"    
+import store from "@/store.js"
+import GoBack from "@/components/GoBack"    
 export default {
+    components: {
+        GoBack
+    },
     data() {
         return {}
     },
